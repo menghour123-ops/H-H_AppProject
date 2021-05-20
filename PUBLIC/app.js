e51d5fc8bc15f79c52c696c1d407e7f4caa03cdb
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2021-05-20 10:39:02 +0700

    try

--- PUBLIC/app.js
+++ PUBLIC/app.js
@@ -1,10 +1,18 @@
 function clickIt(event){
     event.preventDefault();
     console.log("HELLO SECOND TIME")
+    let li = document.createElement("li");
+    UL.appendChild(li);
+    let p = document.createElement("p");
+    li.appendChild(p);
+    p.className = "chart";
+    p.textContent = text.value;
+
 }
 
 // get ul from html
 const UL = document.querySelector("ul");
 // get button and add evenlistener
+const text = document.querySelector("#text");
 const  sendIt = document.getElementById("submit");
 sendIt.addEventListener("click", clickIt);
